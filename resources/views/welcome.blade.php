837715f915986b49d496ed2af47939b42805c50c
TKG-happy <73483449+TKG-happy@users.noreply.github.com>
2021-11-03 15:26:48 +0900

    確認用のコードを消す #3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,6 @@
         </style>
     </head>
     <body class="antialiased">
-        {{-- 確認用にとりあえず表示させている --}}
-        <i class="fas fa-angle-double-up text-7xl text-red-400"></i>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
