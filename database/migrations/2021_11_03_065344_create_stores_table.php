d8328f5d00ec8823ed7d676d90ffaea1032a7272
Tanaka <tanakanaoya@MacBook-Pro.local>
2021-11-05 00:10:56 +0900

    データベース構築 #5

--- database/migrations/2021_11_03_065344_create_stores_table.php
+++ database/migrations/2021_11_03_065344_create_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateStoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,23 +13,23 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->comment('氏名');
-            $table->string('email')->unique()->comment('メールアドレス');
+            $table->string('name')->comment('店舗名');
+            $table->string('email')->comment('メールアドレス');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->comment('パスワード');
-            $table->string('nick_name', 50)->nullable()->comment('表示名');
-            $table->string('icon')->default("UserDefault")->comment('アイコン');
+            $table->string('image')->default("StoreDefault")->comment('店舗画像');
             $table->string('tel', 20)->nullable()->comment('電話番号');
+            $table->text('introduction')->nullable()->comment('店舗紹介');
             $table->string('zipcode', 10)->nullable()->comment('郵便番号');
             $table->string('prefecture')->nullable()->comment('都道府県');
             $table->string('city')->nullable()->comment('市区町村');
             $table->string('street_address')->nullable()->comment('番地');
-            $table->tinyInteger('gender')->unsigned()->nullable()->comment('性別 : 男性(1), 女性(2)');
-            $table->date('birthdate')->nullable()->comment('生年月日');
-            $table->rememberToken();
+            $table->text('business_time')->nullable()->comment('営業時間');
+            $table->foreignId('owner_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
+
+            $table->unique(['name', 'email']);
         });
     }
 
@@ -40,6 +40,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stores');
     }
 }
---
+++ database/migrations/2021_11_03_072444_create_coupons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('クーポン名');
+            $table->text('description')->comment('クーポン詳細');
+            $table->string('code')->comment('クーポン番号');
+            $table->foreignId('product_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
