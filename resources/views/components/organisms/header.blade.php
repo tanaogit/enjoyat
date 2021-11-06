75f747321ac248f9f8180f1522e695696c7701df
TKG-happy <73483449+TKG-happy@users.noreply.github.com>
2021-11-07 03:12:25 +0900

    コメント分の修正分2

--- resources/views/components/organisms/header.blade.php
+++ resources/views/components/organisms/header.blade.php
@@ -12,3 +12,34 @@
         </div>
     </div>
 </header>
+{{-- SP表示におけるメニュー(ハンバーガーメニュークリック時に表示) --}}
+<div id="pagesTemplateSpMenus" class="hidden bg-gray-200 h-20">
+    <ul class="text-center">
+        {{-- 確認用(認証機能ができたときに消す) --}}
+        <li class="pt-2 pb-2 hover:bg-gray-300">
+            無料会員登録
+        </li>
+        <li class="pt-2 pb-2 hover:bg-gray-300">
+            お店をお持ちの方はこちら
+        </li>
+        {{-- 認証機能ができたときに作る --}}
+        {{-- <li class="p-4 mr-2 cursor-pointer font-bold">
+            @if (Route::has('user.login'))
+                @auth('users')
+                    <a href="{{ route('user.dashboard') }}" class="bg-red-300 hover:bg-red-500 rounded-lg p-5 text-sm text-gray-700 dark:text-gray-500 underline">マイページへ</a>
+                @else
+                    <a href="{{ route('user.login') }}" class="bg-red-300 hover:bg-red-500 rounded-lg p-5 text-sm text-gray-700 dark:text-gray-500">無料会員登録</a>
+                @endauth
+            @endif
+        </li>
+        <li class="p-4 cursor-pointer font-bold">
+            @if (Route::has('owner.login'))
+                @auth('owners')
+                    <a href="{{ route('owner.dashboard') }}" class="bg-red-300 hover:bg-red-500 rounded-lg p-5 text-sm text-gray-700 dark:text-gray-500 underline">店舗管理画面へ</a>
+                @else
+                    <a href="{{ route('owner.login') }}" class="bg-red-300 hover:bg-red-500 rounded-lg p-5 text-sm text-gray-700 dark:text-gray-500">お店をお持ちの方</a>
+                @endauth
+            @endif
+        </li> --}}
+    </ul>
+</div>
