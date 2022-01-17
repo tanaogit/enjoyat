1a22dbb985f448f53c02a926779dbcbc0af79724
Tanaka <tanakanaoya@MacBook-Pro.local>
2022-01-17 23:51:41 +0900

    シンプルサーチとディティールサーチ処理の変更 #93

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,6 +11,7 @@ class SearchController extends Controller
 {
     public function simplesearch(Request $request)
     {
+        $display_count = 0;
         if (is_mobile($_SERVER['HTTP_USER_AGENT'])) {
             $display_count = 6;
         } elseif (is_tablet($_SERVER['HTTP_USER_AGENT'])) {
@@ -19,13 +20,14 @@ class SearchController extends Controller
             $display_count = 18;
         }
 
-        $search_stores = Store::AccessFilter($request->pref, $request->line, $request->station)->Paginate($display_count);
+        $search_stores = Store::AccessFilter($request->pref, $request->line, $request->station)->latest('updated_at')->Paginate($display_count)->withQueryString();
 
         return view('search.simplesearch', compact('search_stores'));
     }
 
     public function detailsearch(Request $request)
     {
+        $display_count = 0;
         if (is_mobile($_SERVER['HTTP_USER_AGENT'])) {
             $display_count = 6;
         } elseif (is_tablet($_SERVER['HTTP_USER_AGENT'])) {
@@ -39,24 +41,23 @@ class SearchController extends Controller
         if (!empty($request->payments)) {
             $payments = array_column(Payment::whereIn('id', $request->payments)->get(['method'])->toArray(), 'method');
         }
-        
+
         // 検索条件に合致するジャンルを取得
         $genres = '';
         if (!empty($request->genres)) {
             $genres = array_column(Genre::whereIn('id', $request->genres)->get(['name'])->toArray(), 'name');
         }
 
-        $businessdays = $request->businessdays;
         $search_stores = Store::AccessFilter($request->pref, $request->line, $request->station)
                             ->FreeWordFilter($request->freeword)
-                            ->where(function ($query) use ($businessdays) {
-                                $query->BusinessDaysFilter($businessdays);
-                            })
+                            ->BusinessDaysFilter($request->businessdays)
                             ->EvaluationFilter($request->evaluation)
                             ->PaymentsFilter($request->payments)
                             ->CouponFilter($request->coupon)
                             ->GenresFilter($request->genres)
-                            ->Paginate($display_count);
+                            ->latest('updated_at')
+                            ->Paginate($display_count)
+                            ->withQueryString();
 
         return view('search.detailsearch', compact('payments', 'genres', 'search_stores'));
     }
