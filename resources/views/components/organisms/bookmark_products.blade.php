cc4a278ccc27f57fa6a6c2e3b2ae1883234e5bb7
TKG-happy <73483449+TKG-happy@users.noreply.github.com>
2022-02-05 03:19:41 +0900

    店舗詳細画面の作成 #118

--- resources/views/components/organisms/bookmark_products.blade.php
+++ resources/views/components/organisms/bookmark_products.blade.php
@@ -13,7 +13,13 @@
                     <div class="flex justify-between mt-1">
                         <span class="text-gray-500 text-sm">
                             {{ $bookmark->price }}円<br>
-                            (1回あたり{{ $bookmark->unitprice }}円)
+                            @php
+                                $unitprice = '-';
+                                if (!empty($bookmark->unitprice)) {
+                                    $unitprice = $bookmark->unitprice;
+                                }
+                            @endphp
+                            (1回あたり{{ $unitprice }}円)
                         </span>
                         <span class="text-md font-bold text-red-500">注目</span>
                     </div>
