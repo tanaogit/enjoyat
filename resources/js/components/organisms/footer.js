c588472054323553e76f309e6e952f5a269accdc
TKG-happy <73483449+TKG-happy@users.noreply.github.com>
2022-02-15 17:57:59 +0900

    フッターのコピーライトの年号部分の修正(バグ) #133

--- resources/js/components/organisms/footer.js
+++ resources/js/components/organisms/footer.js
@@ -1,7 +1,5 @@
 // フッターに関する処理
-$(function() {
-    // 今年の西暦をcopyrightの部分に表示
-    const date     = new Date();
-    const thisYear = date.getFullYear();
-    $('#footer-copyright-year').text(thisYear);
-});
+// 一応残しておく
+// $(function() {
+
+// });
