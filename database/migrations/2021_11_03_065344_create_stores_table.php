75e64dc910b07ef29f23066b1e3081913d82f3c4
Tanaka <tanakanaoya@MacBook-Pro.local>
2022-03-04 16:44:42 +0900

    Storesテーブルの住所関連属性を全て非Nullに変更 #127

--- database/migrations/2021_11_03_065344_create_stores_table.php
+++ database/migrations/2021_11_03_065344_create_stores_table.php
@@ -15,16 +15,16 @@ class CreateStoresTable extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->comment('店舗名');
-            $table->string('email')->comment('メールアドレス');
+            $table->string('name', 100)->comment('店舗名');
+            $table->string('email', 254)->comment('メールアドレス');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('image')->default("storage/storeimages/default.png")->comment('店舗画像');
-            $table->string('tel', 20)->nullable()->comment('電話番号');
+            $table->string('tel', 20)->comment('電話番号');
             $table->text('introduction')->nullable()->comment('店舗紹介');
-            $table->string('zipcode', 10)->nullable()->comment('郵便番号');
-            $table->string('prefecture')->nullable()->comment('都道府県');
-            $table->string('city')->nullable()->comment('市区町村');
-            $table->string('street_address')->nullable()->comment('番地');
+            $table->string('zipcode', 10)->comment('郵便番号');
+            $table->string('prefecture', 65)->comment('都道府県');
+            $table->string('city', 65)->comment('市区町村');
+            $table->string('street_address', 65)->comment('番地');
             $table->text('business_time')->nullable()->comment('営業時間');
             $table->foreignId('owner_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
