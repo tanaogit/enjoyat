d3890f9180268d7c748005c9a92bb23c5a445664
Tanaka <tanakanaoya@MacBook-Pro.local>
2022-03-10 19:43:12 +0900

    ユーザーが認証済状態でトップページ画面を表示した際、あなたへのおすすめサブスクを表示する機能を追加 #142

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
@@ -48,5 +49,51 @@ class Product extends Model
     public function bookmarkUsers()
     {
         return $this->belongsToMany(User::class)->withPivot('category')->wherePivot('category', 'bookmark');
+    }
+
+    /**
+     * パラメータに与えられる住所情報に合致する店舗の登録商品のみを抽出する処理
+     * ※パラメータがnullの場合は処理を実行せずreturn
+     *
+     * @param Illuminate\Database\Eloquent\Builder $query
+     * @param string $pref
+     * @param string $city
+     * @param string $street_address
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeStoreAddressFilter($query, $pref, $city, $street_address = null)
+    {
+        if (empty($pref)) return;
+
+        return $query->whereHas('store', function (Builder $query) use ($pref, $city, $street_address) {
+            $query
+                ->when($pref, function ($query, $pref) {
+                    return $query->where('prefecture', $pref);
+                })
+                ->when($city, function ($query, $city) {
+                    return $query->where('city', $city);
+                })
+                ->when($street_address, function ($query, $street_address) {
+                    return $query->where('street_address', $street_address);
+                });
+        });
+    }
+
+    /**
+     * パラメータに与えられるジャンルのいずれか1つ以上が合致する登録商品を全て抽出する処理
+     * ※パラメータに値がセットされていなければ処理を実行せずreturn
+     *
+     * @param Illuminate\Database\Eloquent\Builder $query
+     * @param array $genres
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeGenresFilter($query, $genres)
+    {
+        if (!is_array($genres)) return;
+        if (empty($genres)) return;
+
+        return $query->whereHas('genres', function ($query) use ($genres) {
+            $query->whereIn('id', $genres);
+        });
     }
 }
